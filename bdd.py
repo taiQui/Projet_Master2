27c8d465a477a532aa5aa46303ab72793c623d2b
taiQui <greg1310@hotmail.fr>
2020-01-19 11:50:29 +0100

    adding com to bdd.py

--- bdd.py
+++ bdd.py
@@ -1,14 +1,31 @@
 #!/bin/env python3
 # coding : utf-8
+
+
+"""
+Class to use mysql Database with python3
+simple to use
+--------------
+    database = DB(user="root",password="root",db="YourDatabase",host="localhost")
+"""
+
+
+
 import mysql.connector,string,random,hashlib
 MAX_USER = 20
 
 def generate_random_password():
     return ''.join(random.choice(string.ascii_letters+string.digits) for i in range(32))
 
+
+
+
 class DB:
 
     def __init__(self,user,password,db,host="localhost"):
+        """
+            Define DB class
+        """
         self.username = user
         self.password = password
         self.host     = host
@@ -28,6 +45,9 @@ class DB:
             print("[-] Error while attempting to connect to the database\n{}".format(err))
 
     def __repr__(self):
+        """
+            Allow to "print" DB instance
+        """
         tables = self.getTables()
         pt = "Tables : "
         for table in tables:
@@ -35,19 +55,34 @@ class DB:
         return "Database : "+self.dbname+"\n"+pt
 
     def getTables(self):
+        """
+            Return All table from the database
+        """
         self.cursor.execute("show tables;")
         return self.cursor.fetchall()
 
     def show_table(self):
+        """
+            Print all tables
+        """
         tables = self.getTables()
         for table in tables:
             print(table)
 
     def getUsers(self):
+        """
+            Return All user from Table Users
+        """
         self.cursor.execute("select * from users;")
         users = self.cursor.fetchall()[1:]
         return users
     def createUser(self):
+        """
+            Create User to use application
+            Name would be userX with X in range 1 and MAX_USER
+            password is generated randomly with lower,upper,digit with 32 length
+            Add user to the database and return creds with tuple
+        """
         users = self.getUsers()
         name = ""
         if len(users) > MAX_USER:
@@ -67,6 +102,9 @@ class DB:
                 print("[-] Error while inserting row to the database.\n{}".format(err))
                 return (None,None)
     def deleteUser(self,name):
+        """
+            Delete user and print how many line has been removed
+        """
         try:
             self.cursor.execute("delete from users where name=\'"+name+"\'")
             self.database.commit()
@@ -75,6 +113,9 @@ class DB:
             print("[-] Error while deleting row to the database.\n{}".format(err))
 
     def auth(self,username,password):
+        """
+            Test if given credential is in database, return True or False
+        """
         cipher = hashlib.md5()
         cipher.update(password.encode())
         try:
@@ -85,6 +126,9 @@ class DB:
             print("[-] Error while attempting to execute select statement.\n{}".format(err))
 
     def getPoints(self,user):
+        """
+            Return point for given user
+        """
         try:
             self.cursor.execute("select points from users where name=\'"+user+"\'")
             r = self.cursor.fetchall()
@@ -95,6 +139,9 @@ class DB:
             print("[-] Error while getting point\n{}".format(err))
 
     def cleanAll(self):
+        """
+            Remove all user except admin from the database
+        """
         try:
             users = self.getUsers()
             for user in users:
@@ -106,6 +153,9 @@ class DB:
             print("[-] Error while cleaning database.\n{}".format(err))
 
     def addPoint(self,name,point):
+        """
+            Add given point to given user
+        """
         try:
             pt = int(self.getPoints(name))
             self.cursor.execute("update users set points="+str(point+pt)+" where name=\'"+name+"\'")
@@ -115,6 +165,9 @@ class DB:
             print("[-] Error while adding point to {}.\n{}".format(name,err))
 
     def removePoint(self,name,point):
+        """
+            Remove given point to given user
+        """
         try:
             pt = int(self.getPoints(name))
             self.cursor.execute("update users set points="+str(pt-point)+" where name=\'"+name+"\'")
