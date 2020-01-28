1e35953841ff248715c3ae5864567773fa511758
taiQui <greg1310@hotmail.fr>
2020-01-28 17:05:25 +0100

    adding flag submission

--- site_owasp/pages/urls.py
+++ site_owasp/pages/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home_view,name='home'),
     path('profil/',views.profil),
+    path('flag',views.flag),
     # path('demarageChallenge/', view/s.lancerMachine),
 ]
